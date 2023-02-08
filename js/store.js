76713252becd3a462d1ab14d859d20229227a7c2
Kian Kerby Adrisola <adrisolakian@gmail.com>
2023-02-08 16:37:22 +0800

    fg
    sdfgdg

--- js/store.js
+++ js/store.js
@@ -1,12 +1,12 @@
 // modules
-import gemma from './modules/gemma.js';
+import Kian from './modules/Kian.js';
 
 // import your .js file here (e.g. import john from './modules/john.js')
 // ...
 
 const store = Vuex.createStore({
     modules: {
-        gemma,
+        Kian,
 
         // include your module here (e.g. john)
         // ...
